21f04b3adf220069a1e4587b361e1bd468730dcd
WillyAgustri <willyofficial082@gmail.com>
2023-12-24 02:02:38 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import './student.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 36, 133, 218)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Student> students = [
+    Student(id: "1", name: "Willy", age: 20, date: DateTime.now()),
+    Student(id: "2", name: "Bowo", age: 20, date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('List View'),
+      ),
+      body: ListView(
+        children: students.map((st) {
+          return Card(
+            child: Row(
+              children: [Text(st.name)],
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
---
+++ lib/student.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// ignore: empty_constructor_bodies
+class Student {
+  final String id;
+  final String name;
+  final int age;
+  final DateTime date;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.date,
+  });
+}
